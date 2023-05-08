34325ecd94f76321019df61e13e7b9a895cbba0c
pabloponc <pablitooponcee@gmail.com>
2023-05-08 15:55:41 -0300

    Homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,5 +1,6 @@
 "use strict";
 
+ 
 function BinarioADecimal(num) {
   // 011001 = 25
   // <------- leyendo de der a izq
@@ -9,20 +10,33 @@ function BinarioADecimal(num) {
   //    1 * 2 ** 3  =  8
   //    1 * 2 ** 4  =  16
   //    0 * 2 ** 5  =   0
+ 
+  var suma = 0;
+  let digitos = num.toString ().split('').reverse().join('');
+  for (let i=0; i<digitos.length; i++) {
+    suma += ((digitos[i])* Math.pow(2, i));
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
-  // 91 = 1011011
-  // 91 / 2 = 45  (.5) ===> 1
-  // 45 / 2 = 22   (.5) ===> 1
-  // 22 / 2 = 11  (0) ===> 0
-  // 91 / 2 = 45 residuo 1
-  // 45 / 2 = 22 residuo 1
-  // 22 / 2 = 11 residuo 0
-  // 11/ 2 = 5 residuo 1
-  // 5 / 2 = 2 residuo 1
-  // 2 / 2 = 1 residuo 0
-  // 1 / 2 = 0 residuo 1
+    // 91 = 1011011
+    // 91 / 2 = 45 residuo 1
+    // 45 / 2 = 22 residuo 1
+    // 22 / 2 = 11 residuo 0
+    // 11/ 2 = 5 residuo 1
+    // 5 / 2 = 2 residuo 1
+    // 2 / 2 = 1 residuo 0
+    // 1 / 2 = 0 residuo 1
+  var numBinario = [];
+  for (let i=0; num>0; i++){
+    if (num % 2 != 0){
+      numBinario.unshift(1);
+    }
+    else numBinario.unshift(0);
+    num = Math.floor(num/2);
+  }
+  return numBinario.join('');
 }
 
 module.exports = {
