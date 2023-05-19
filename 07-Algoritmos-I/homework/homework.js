e1f76a38fbaaeb940025af139ce9b4c364a4fdf5
pabloponc <pablitooponcee@gmail.com>
2023-05-18 23:46:57 -0300

    algoritmoI

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,32 +7,74 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  let divisores = [1];
+  let divisor = 2;
+  
+  while (num > 1 ) {
+    if (num % divisor === 0) {
+      divisores.push(divisor);
+      num = num / divisor;
+    } else divisor++;
+  }
+  return divisores;
 }
 
-function bubbleSort(array) {
+function bubbleSort(array) {  //VA ORDENANDO POR PAREJAS
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let ordenado = false;
 
+  while (!ordenado) {
+    ordenado = true;
+    for (let i=0; i<array.length - 1; i++) {
+      if(array[i] > array[i + 1]) {
+        let aux = array[i];
+        array [i] = array[i + 1];
+        array [i + 1] = aux;
+        ordenado = false;
+      }
+    }
+  }
+  return array;
 }
 
-
-function insertionSort(array) {
+function insertionSort(array) {  // VA COMPARANDO Y ORDENANDO HACIA ATRAS
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
 
+  for (let i=1; i<array.length; i++) {
+    let aux = array[i];
+    let j = i - 1;
+    while (j >= 0 && aux < array[j]) {
+      array [j + 1] = array [j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
 
-
-function selectionSort(array) {
+function selectionSort(array) {  // BUSCA EL MENOR DE TODOS LOS ELEMENTOS Y LUEGO COMPARA Y ENCUENTRA EL LUGAR QUE LE CORRESPONDE
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array [j] < array[minIndex]) minIndex = j;
+    }
+    if (minIndex !==i) {
+      let aux = array [i];
+      array [i] = array [minIndex];
+      array [minIndex] = aux;
+    } 
+  }
+  return array;
 }
 
 
