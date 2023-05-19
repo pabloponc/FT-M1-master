751cec9611825c794140c3200edfb8c663d93e6a
pabloponc <pablitooponcee@gmail.com>
2023-05-19 00:25:21 -0300

    algoritmoII

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -7,6 +7,23 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length <= 1) return array;
+
+  let pivot = array[Math.floor(Math.random() * array.length)]
+
+  let equals = []; 
+  let left = [];
+  let right = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== pivot){
+      if (array[i] < pivot) left.push(array[i])
+      else right.push(array[i]);
+    } 
+    else equals.push(array[i]);
+  }
+  return quickSort(left).concat(equals).concat(quickSort(right));
+
 }
 
 function mergeSort(array) {
@@ -14,6 +31,25 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+
+  if (array.length<2) return array;
+
+  let mitad = Math.floor(array.length / 2);
+  let left = array.slice(0,mitad);
+  let right = array.slice(mitad);
+
+  array = [];
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  while(left.length && right.length) {
+    if (left[0] < right[0]) array.push(left.shift());
+    else array.push(right.shift());
+  }
+  return array.concat(left,right);
+
+
 
   //okey probemos ahora
 }
